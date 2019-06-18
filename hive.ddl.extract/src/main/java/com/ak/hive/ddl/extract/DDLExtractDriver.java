5050db24e0d45898fd07871ce8ea1ffb539f09c0
Arun A K <arun.cetly@gmail.com>
2019-06-18 10:54:12 -0400

    1
    1

---
+++ hive.ddl.extract/src/main/java/com/ak/hive/ddl/extract/DDLExtractDriver.java
@@ -0,0 +1,16 @@
+package com.ak.hive.ddl.extract;
+
+public class DDLExtractDriver {
+
+	
+	public static void main(String[] args) {
+		
+		
+		HiveDDLExtractor extractor = new HiveDDLExtractor();
+		extractor.extract("filename");
+		
+		
+	}
+	
+	
+}
