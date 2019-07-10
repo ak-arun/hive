3a6bdfc291dec620cae768b44bf03b044da6bdd4
Arun A K <arun.cetly@gmail.com>
2019-07-10 12:56:58 -0400

    async notif

--- ddlgrabber/src/main/java/com/ak/hive/ddlgrabber/hook/HiveDDLGrabberHook.java
+++ ddlgrabber/src/main/java/com/ak/hive/ddlgrabber/hook/HiveDDLGrabberHook.java
@@ -10,29 +10,22 @@ import java.util.Map;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.LinkedBlockingDeque;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
 
 import org.apache.hadoop.hive.conf.HiveConf;
 import org.apache.hadoop.hive.ql.hooks.ExecuteWithHookContext;
 import org.apache.hadoop.hive.ql.hooks.HookContext;
 import org.apache.hadoop.hive.ql.hooks.WriteEntity;
 import org.apache.hadoop.security.UserGroupInformation;
-import org.apache.hadoop.util.ShutdownHookManager;
-import org.apache.kafka.clients.producer.Callback;
-import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
-import org.apache.kafka.clients.producer.RecordMetadata;
 import org.apache.kafka.common.config.SaslConfigs;
 import org.apache.kafka.common.config.SslConfigs;
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.ak.hive.ddlgrabber.util.DDLGrabberConstants;
 import com.ak.hive.ddlgrabber.util.DDLHookNotificationHandler;
-import com.ak.hive.ddlgrabber.util.Utils;
-import com.google.common.util.concurrent.ThreadFactoryBuilder;
 
 
 public class HiveDDLGrabberHook implements ExecuteWithHookContext {
@@ -43,9 +36,7 @@ public class HiveDDLGrabberHook implements ExecuteWithHookContext {
 	private String databaseName;
 	private HiveConf configuration;
 	private Map<String, Object> propertyMap;
-	//private KafkaProducer<String, String> producer;
-	private String notification;
-	//private ExecutorService executor = null;
+	private JSONObject notificationObject;
 	private ExecutorService executorService = null;
 
 	
@@ -72,18 +63,9 @@ public class HiveDDLGrabberHook implements ExecuteWithHookContext {
 			propertyMap.put(DDLGrabberConstants.SASL_KERBEROS_SERVICE_NAME,configuration.get(DDLGrabberConstants.DDL_HOOK_KAFKA_SERVICE_NAME));
 			propertyMap.put(DDLGrabberConstants.SECURITY_PROTOCOL, configuration.get(DDLGrabberConstants.DDL_HOOK_KAFKA_SECURITY_PROTOCOL));
 			
-			//int maxNotifThreads = configuration.getInt(DDLGrabberConstants.DDL_HOOK_NOTIFICATION_MAX_POOL_SIZE, 1);
 			
 			
 			executorService = Executors.newFixedThreadPool(1);
-			
-			/*
-			int minNotifThreads = configuration.getInt(DDLGrabberConstants.DDL_HOOK_NOTIFICATION_MIN_THREAD, 1);
-			int maxNotifThreads = configuration.getInt(DDLGrabberConstants.DDL_HOOK_NOTIFICATION_MAX_THREAD, 1);
-			long keepAliveTimeMs = configuration.getLong(DDLGrabberConstants.DDL_HOOK_NOTIFICATION_KEEP_ALIVE_TIME_MS, 10000);
-			int  queueSize       = configuration.getInt(DDLGrabberConstants.DDL_HOOK_NOTIFICATION_QUEUE_SIZE, 10000);
-			
-			*/
 			
 			/*
 			 * Dynamic JAAS Configuration as in
@@ -93,31 +75,6 @@ public class HiveDDLGrabberHook implements ExecuteWithHookContext {
 			
 			if(isDDL()){
 				
-				/*executor = new ThreadPoolExecutor(minNotifThreads, maxNotifThreads, keepAliveTimeMs, TimeUnit.MILLISECONDS, new LinkedBlockingDeque<Runnable>(queueSize), new ThreadFactoryBuilder().setNameFormat("DDL Hook Notifier %d ").setDaemon(true).build());
-				
-				ShutdownHookManager.get().addShutdownHook(new Thread() {
-					@Override
-					public void run() {
-						try{
-							executor.shutdown();
-			                executor.awaitTermination(DDLGrabberConstants.SHUTDOWN_HOOK_WAIT_TIME_MS, TimeUnit.MILLISECONDS);
-			                executor = null;
-			            }
-						catch(Exception e){
-							LOG.info("Exception in shutdown "+Utils.getTraceString(e));
-							} 
-						finally {
-							LOG.info("Executor Shutdown");
-						}
-					}
-					
-				}, DDLGrabberConstants.SHUTDOWN_HOOK_PRIORITY);
-				*/
-				
-			/*	for(WriteEntity output : hookContext.getOutputs()){
-					databaseName = databaseName==null?(output.getDatabase()!=null?output.getDatabase().getName():null):databaseName;
-					tableName = tableName==null?(output.getTable()!=null?output.getTable().getTableName():null):tableName;
-				}*/
 			
 			final ProducerRecord<String, String> record = generateNotificationRecord(hookContext);
 			
@@ -138,8 +95,6 @@ public class HiveDDLGrabberHook implements ExecuteWithHookContext {
 					}
 				});
 				
-				//send(record);
-				
 			}
 			else{
 				propertyMap.put(SaslConfigs.SASL_JAAS_CONFIG,DDLGrabberConstants.JAAS_CONFIG_NO_KEYTAB
@@ -151,17 +106,6 @@ public class HiveDDLGrabberHook implements ExecuteWithHookContext {
 			}
 	}
 	
-	/*private void send(ProducerRecord<String, String> record){
-		producer = new KafkaProducer<String,String>(propertyMap);
-		producer.send(record , new Callback() {
-			public void onCompletion(RecordMetadata metadata, Exception exception) {
-				if(exception!=null){
-					exception.printStackTrace();
-				}
-			}
-		});
-		producer.close();
-	}*/
 	private boolean isDDL() {
 		try{
 			return query.toLowerCase().trim().startsWith(DDLGrabberConstants.CREATE)||query.toLowerCase().trim().startsWith(DDLGrabberConstants.ALTER) ? true : false;
@@ -185,7 +129,6 @@ public class HiveDDLGrabberHook implements ExecuteWithHookContext {
 		}
 
 		public Object run() throws Exception {
-			//send(record);
 			executorService.submit(new Callable<Object>() {
 				public Object call() throws Exception {
 					new DDLHookNotificationHandler(propertyMap, record).send();
@@ -195,12 +138,16 @@ public class HiveDDLGrabberHook implements ExecuteWithHookContext {
 			return null;
 		}
 	}
-	private ProducerRecord<String, String> generateNotificationRecord(HookContext hookContext){
+	private ProducerRecord<String, String> generateNotificationRecord(HookContext hookContext) throws JSONException{
+		notificationObject = new JSONObject();
 		for(WriteEntity output : hookContext.getOutputs()){
 			databaseName = databaseName==null?(output.getDatabase()!=null?output.getDatabase().getName():null):databaseName;
 			tableName = tableName==null?(output.getTable()!=null?output.getTable().getTableName():null):tableName;
 		}
-		notification = "_database="+databaseName+"_table="+tableName+"_datetime="+new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS").format(new Date())+"_query="+query;
-		return new ProducerRecord<String, String>(configuration.get(DDLGrabberConstants.DDL_HOOK_KAFKA_TOPIC_NAME), notification);
+		notificationObject.put("database", databaseName);
+		notificationObject.put("table", tableName);
+		notificationObject.put("dateTime", new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS").format(new Date()));
+		notificationObject.put("ddl", query);
+		return new ProducerRecord<String, String>(configuration.get(DDLGrabberConstants.DDL_HOOK_KAFKA_TOPIC_NAME), notificationObject.toString());
 	}
 }
