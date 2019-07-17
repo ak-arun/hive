a49337ef2ef53ba8884041f684c7ab8d8319e1fc
Arun A K <arun.cetly@gmail.com>
2019-07-17 12:25:59 -0400

    query hook

---
+++ ddlparent/ddlhook/src/main/java/com/ak/hive/ddlgrabber/hook/QueryHook.java
@@ -0,0 +1,171 @@
+package com.ak.hive.ddlgrabber.hook;
+
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hive.conf.HiveConf;
+import org.apache.hadoop.hive.ql.QueryPlan;
+import org.apache.hadoop.hive.ql.exec.ExplainTask;
+import org.apache.hadoop.hive.ql.exec.Task;
+import org.apache.hadoop.hive.ql.exec.Utilities;
+import org.apache.hadoop.hive.ql.hooks.ExecuteWithHookContext;
+import org.apache.hadoop.hive.ql.hooks.HookContext;
+import org.apache.hadoop.util.StringUtils;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
+
+
+public class QueryHook implements ExecuteWithHookContext {
+
+  private static final Log LOG = LogFactory.getLog(QueryHook.class.getName());
+  private static final Object LOCK = new Object();
+  private static ExecutorService executor;
+  private enum EntityTypes { HIVE_QUERY_ID };
+  private enum EventTypes { QUERY_SUBMITTED, QUERY_COMPLETED };
+  private enum OtherInfoTypes { QUERY, STATUS, TEZ, MAPRED };
+  private enum PrimaryFilterTypes { user, requestuser, operationid };
+  private static final int WAIT_TIME = 3;
+
+  public QueryHook() {
+    synchronized(LOCK) {
+      if (executor == null) {
+        executor = Executors.newSingleThreadExecutor(
+           new ThreadFactoryBuilder().setDaemon(true).setNameFormat("QueryHook Logger %d").build());
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+          @Override
+          public void run() {
+            try {
+              executor.shutdown();
+              executor.awaitTermination(WAIT_TIME, TimeUnit.SECONDS);
+              executor = null;
+            } catch(InterruptedException ie) { }
+          }
+        });
+      }
+    }
+
+    LOG.info("Created Query Hook");
+  }
+
+  @Override
+  public void run(final HookContext hookContext) throws Exception {
+    final long currentTime = System.currentTimeMillis();
+    final HiveConf conf = new HiveConf(hookContext.getConf());
+
+    executor.submit(new Runnable() {
+        @Override
+        public void run() {
+          try {
+            QueryPlan plan = hookContext.getQueryPlan();
+            if (plan == null) {
+              return;
+            }
+            String queryId = plan.getQueryId();
+            String opId = hookContext.getOperationId();
+            long queryStartTime = plan.getQueryStartTime();
+            String user = hookContext.getUgi().getUserName();
+            String requestuser = hookContext.getUserName();
+            if (hookContext.getUserName() == null ){
+            	requestuser = hookContext.getUgi().getUserName() ; 
+            }
+            int numMrJobs = Utilities.getMRTasks(plan.getRootTasks()).size();
+            int numTezJobs = Utilities.getTezTasks(plan.getRootTasks()).size();
+            if (numMrJobs + numTezJobs <= 0) {
+              return; 
+            }
+
+            switch(hookContext.getHookType()) {
+            case PRE_EXEC_HOOK:
+              ExplainTask explain = new ExplainTask();
+              explain.initialize(conf, plan, null);
+              String query = plan.getQueryStr();
+              List<Task<?>> rootTasks = plan.getRootTasks();
+              JSONObject explainPlan = explain.getJSONPlan(null, null, rootTasks,
+                   plan.getFetchTask(), true, false, false);
+              fireAndForget(conf, createPreHookEvent(queryId, query,
+                   explainPlan, queryStartTime, user, requestuser, numMrJobs, numTezJobs, opId));
+              break;
+            case POST_EXEC_HOOK:
+              fireAndForget(conf, createPostHookEvent(queryId, currentTime, user, requestuser, true, opId,"post"));
+              break;
+            case ON_FAILURE_HOOK:
+              fireAndForget(conf, createPostHookEvent(queryId, currentTime, user, requestuser , false, opId,"fail"));
+              break;
+            default:
+              //ignore
+              break;
+            }
+          } catch (Exception e) {
+            LOG.info("Failed to submit plan: " + StringUtils.stringifyException(e));
+          }
+        }
+      });
+  }
+
+  JSONObject createPreHookEvent(String queryId, String query, JSONObject explainPlan,
+      long startTime, String user, String requestuser, int numMrJobs, int numTezJobs, String opId) throws Exception {
+
+    JSONObject queryObj = new JSONObject();
+    queryObj.put("hookType", "pre");
+    queryObj.put("queryText", query);
+    queryObj.put("queryPlan", explainPlan);
+
+    LOG.info("Received pre-hook notification for :" + queryId);
+    if (LOG.isDebugEnabled()) {
+      LOG.debug("Otherinfo: " + queryObj.toString());
+      LOG.debug("Operation id: <" + opId + ">");
+    }
+
+    
+    queryObj.put("entityId", queryId);
+    queryObj.put("entityType", EntityTypes.HIVE_QUERY_ID.name());
+    queryObj.put(PrimaryFilterTypes.user.name(), user);
+    queryObj.put(PrimaryFilterTypes.requestuser.name(), requestuser);
+    
+    if (opId != null) {
+    	queryObj.put(PrimaryFilterTypes.operationid.name(), opId);
+    }
+    queryObj.put("eventType", EventTypes.QUERY_SUBMITTED.name());
+    queryObj.put("eventTimestamp", startTime);
+    queryObj.put(OtherInfoTypes.TEZ.name(), numTezJobs > 0);
+    queryObj.put(OtherInfoTypes.MAPRED.name(), numMrJobs > 0);
+    
+    
+    return queryObj;
+  }
+
+  JSONObject createPostHookEvent(String queryId, long stopTime, String user, String requestuser, boolean success,
+      String opId, String type) throws JSONException {
+    LOG.info("Received post-hook notification for :" + queryId);
+   
+    JSONObject queryObj = new JSONObject();
+    queryObj.put("hookType", type);
+
+    queryObj.put("entityId", queryId);
+    queryObj.put("entityType", EntityTypes.HIVE_QUERY_ID.name());
+    
+    queryObj.put(PrimaryFilterTypes.user.name(), user);
+    queryObj.put(PrimaryFilterTypes.requestuser.name(), requestuser);
+    if (opId != null) {
+    	queryObj.put(PrimaryFilterTypes.operationid.name(), opId);
+    }
+
+    queryObj.put("eventType",EventTypes.QUERY_COMPLETED.name());
+    queryObj.put("eventTimestamp",stopTime);
+
+    queryObj.put(OtherInfoTypes.STATUS.name(), success);
+    return queryObj;
+  }
+
+  synchronized void fireAndForget(Configuration conf, JSONObject queryObj) throws Exception {
+	  System.out.println("Notification Triggered");
+	  System.out.println("Event Notification : "+ queryObj.toString());
+  }
+}
