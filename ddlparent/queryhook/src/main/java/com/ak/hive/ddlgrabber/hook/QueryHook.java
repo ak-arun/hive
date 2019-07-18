1a53edf015832acc8001d65cf52c6046fa663d23
Arun A K <arun.cetly@gmail.com>
2019-07-18 10:17:38 -0400

    ddl

--- ddlparent/queryhook/src/main/java/com/ak/hive/ddlgrabber/hook/QueryHook.java
+++ ddlparent/queryhook/src/main/java/com/ak/hive/ddlgrabber/hook/QueryHook.java
@@ -1,21 +1,29 @@
 package com.ak.hive.ddlgrabber.hook;
 
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+import java.util.Set;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hive.conf.HiveConf;
 import org.apache.hadoop.hive.ql.QueryPlan;
 import org.apache.hadoop.hive.ql.exec.Utilities;
 import org.apache.hadoop.hive.ql.hooks.ExecuteWithHookContext;
 import org.apache.hadoop.hive.ql.hooks.HookContext;
+import org.apache.hadoop.hive.ql.hooks.ReadEntity;
+import org.apache.hadoop.hive.ql.hooks.WriteEntity;
+import org.apache.hadoop.hive.ql.metadata.Table;
 import org.apache.hadoop.util.StringUtils;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import com.ak.hive.ddlgrabber.util.DDLGrabberUtils;
 import com.google.common.util.concurrent.ThreadFactoryBuilder;
 
 
@@ -28,6 +36,8 @@ public class QueryHook implements ExecuteWithHookContext {
   private enum OtherInfoTypes { QUERY, STATUS, TEZ, MAPRED };
   private enum PrimaryFilterTypes { user, requestuser, operationid };
   private static final int WAIT_TIME = 3;
+  private static final List<String> DDL_START_WORDS = Arrays.asList(new String [] {"CREATE","ALTER","DROP"});
+  private static final List<String>DB_START_WORDS= Arrays.asList(new String [] {"DATABASE","SCHEMA"});
 
   public QueryHook() {
     synchronized(LOCK) {
@@ -63,65 +73,46 @@ public class QueryHook implements ExecuteWithHookContext {
             if (plan == null) {
               return;
             }
-            String queryId = plan.getQueryId();
             String opId = hookContext.getOperationId();
             long queryStartTime = plan.getQueryStartTime();
             String user = hookContext.getUgi().getUserName();
-            String requestuser = hookContext.getUserName();
-            if (hookContext.getUserName() == null ){
-            	requestuser = hookContext.getUgi().getUserName() ; 
-            }
+            String requestuser = hookContext.getUserName() == null ? user : hookContext.getUserName();
             int numMrJobs = Utilities.getMRTasks(plan.getRootTasks()).size();
             int numTezJobs = Utilities.getTezTasks(plan.getRootTasks()).size();
+            String queryId = plan.getQueryId();
             
-            
-            
+            String queryString = plan.getQueryStr();   
 
             switch(hookContext.getHookType()) {
             case PRE_EXEC_HOOK:
-            	System.out.println("PRE_EXEC_HOOK");
-             // ExplainTask explain = new ExplainTask();
-             // explain.initialize(conf, plan, null);
-              System.out.println("PRE_EXEC_HOOK INIT ExplainTask");
-              String query = plan.getQueryStr();
-              System.out.println("PRE_EXEC_HOOK Query "+query);
-              //List<Task<?>> rootTasks = plan.getRootTasks();
-              System.out.println("PRE_EXEC_HOOK getRootTasks");
-              /*JSONObject explainPlan = explain.getJSONPlan(null, null, rootTasks,
-                   plan.getFetchTask(), true, true, true);*/
-             // System.out.println("PRE_EXEC_HOOK JSONObject explainPlan"+explainPlan);
-              fireAndForget(conf, createPreHookEvent(queryId, query,
-                   null, queryStartTime, user, requestuser, numMrJobs, numTezJobs, opId));
-              System.out.println("PRE_EXEC_HOOK fireAndForget");
+              fireAndForget(generatePreExecutionNotification(queryId,
+                   queryStartTime, user, requestuser, numMrJobs, numTezJobs, opId));
               break;
             case POST_EXEC_HOOK:
-              fireAndForget(conf, createPostOrFailHookEvent(queryId, currentTime, user, requestuser, true, opId));
+              fireAndForget(generatePostExecutionNotification(queryId, currentTime, user, requestuser, true, opId,queryString,hookContext.getOutputs(), hookContext.getInputs()));
               break;
             case ON_FAILURE_HOOK:
-              fireAndForget(conf, createPostOrFailHookEvent(queryId, currentTime, user, requestuser , false, opId));
+              fireAndForget(generatePostExecutionNotification(queryId, currentTime, user, requestuser , false, opId, queryString,hookContext.getOutputs(), hookContext.getInputs()));
               break;
             default:
               break;
             }
           } catch (Exception e) {
-            LOG.info("Failed to submit plan: " + StringUtils.stringifyException(e));
+            LOG.info("Failed to submit plan: "+ StringUtils.stringifyException(e));
           }
         }
       });
   }
 
-  JSONObject createPreHookEvent(String queryId, String query, JSONObject explainPlan,
+  JSONObject generatePreExecutionNotification(String queryId,
       long startTime, String user, String requestuser, int numMrJobs, int numTezJobs, String opId) throws Exception {
 
-	 System.out.println("createPreHookEvent"); 
 	  
     JSONObject queryObj = new JSONObject();
     queryObj.put("hookType", "pre");
-    queryObj.put("queryText", query);
-    queryObj.put("queryPlan", explainPlan);
-
-    LOG.info("Received pre-hook notification for :" + queryId);
+    
     if (LOG.isDebugEnabled()) {
+      LOG.debug("Received pre-hook notification for :" + queryId);
       LOG.debug("Otherinfo: " + queryObj.toString());
       LOG.debug("Operation id: <" + opId + ">");
     }
@@ -139,35 +130,75 @@ public class QueryHook implements ExecuteWithHookContext {
     queryObj.put(OtherInfoTypes.TEZ.name(), numTezJobs > 0);
     queryObj.put(OtherInfoTypes.MAPRED.name(), numMrJobs > 0);
     
+   
     
     return queryObj;
   }
 
-  JSONObject createPostOrFailHookEvent(String queryId, long stopTime, String user, String requestuser, boolean success,
-      String opId) throws JSONException {
-	  System.out.println("createPostOrFailHookEvent"); 
+  JSONObject generatePostExecutionNotification(String queryId, long stopTime, String user, String requestuser, boolean success,
+      String opId, String queryString, Set<WriteEntity> outputs, Set<ReadEntity> inputs) throws JSONException {
     LOG.info("Received post-hook notification for :" + queryId);
    
     JSONObject queryObj = new JSONObject();
     queryObj.put("hookType", success==true?"post":"fail");
-
     queryObj.put("queryId", queryId);
-    
+    queryObj.put("queryText", queryString);
     queryObj.put(PrimaryFilterTypes.user.name(), user);
     queryObj.put(PrimaryFilterTypes.requestuser.name(), requestuser);
     if (opId != null) {
     	queryObj.put(PrimaryFilterTypes.operationid.name(), opId);
     }
-
     queryObj.put("eventType",EventTypes.QUERY_COMPLETED.name());
     queryObj.put("eventTimestamp",stopTime);
-
     queryObj.put(OtherInfoTypes.STATUS.name(), success);
+    
+    if(success&&isDDL(queryString)){
+    	String dbName=null;
+    	String tableName=null;
+    	queryObj.put("isDDL", true);
+    	queryObj.put("dump_time", new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS").format(new Date()));
+    	if(isDBCommand(queryString)){
+    		for(WriteEntity output:outputs){
+    			dbName=dbName==null?(output.getDatabase() == null ? null : output.getDatabase().getName()):dbName;
+    		}
+    		tableName="";
+    	}
+    	
+    	if(dbName==null){
+    		Table table=null;
+    		for(WriteEntity output:outputs){
+    		table = table==null?(output.getTable()!=null?output.getTable():null):table;
+    		}
+    		try{
+    			tableName = table.getTableName();
+    			dbName = table.getDbName();
+    		}catch(Exception e){
+    			tableName = tableName!=null?tableName:"";
+    			dbName=dbName!=null?dbName:"";
+    			LOG.info("Error processing query "+queryId+" exception trace "+DDLGrabberUtils.getTraceString(e));
+    		}
+    	}
+    	queryObj.put("db_name", dbName);
+    	queryObj.put("table_name", tableName);
+    }
     return queryObj;
   }
 
-  synchronized void fireAndForget(Configuration conf, JSONObject queryObj) throws Exception {
-	  System.out.println("Notification Triggered");
+  synchronized void fireAndForget(JSONObject queryObj) throws Exception {
 	  System.out.println("Event Notification : "+ queryObj.toString());
   }
+  
+  private boolean isDDL(String query) {
+		try{
+			return DDL_START_WORDS.contains((query.trim().toUpperCase().split(" "))[0]);
+		}catch(Exception e){}
+		return false;
+	}
+  
+  private boolean isDBCommand(String query) {
+		try{
+			return DB_START_WORDS.contains(((query.trim().toUpperCase().split(" "))[1]).trim());
+		}catch(Exception e){}
+		return false;
+	}
 }
